1f13e5d007dbe884dd4107b89d80fcb48a8de4f1
atalhox <brum.felipe@outlook.com>
2024-02-08 18:24:32 -0300

    First tests

--- assertions.js
+++
@@ -1,1 +0,0 @@
-if (2 === 2) if (15 > 42) if (2 >= 2) ['abacaxi', 'banana', 'uva'].includes('abacaxi');
--- index.js
+++ index.js
@@ -1,50 +1,7 @@
-import assert from 'node:assert/strict';
+const somaHorasExtras = (salario, valorHoraExtra) => salario + valorHoraExtra;
 
-const addOvertime = (salary, overtimeValue) => salary + overtimeValue;
+const calculaDescontos = (salario, deducoes) => salario - deducoes;
 
-const calculateDeductions = (salary, deductions) => salary - deductions;
+const multiplicaDepoisDobra = (valor1, valor2) => (valor1 * valor2) * 2;
 
-/**
- * Generates an assertion that verifies that a given value is an expected value.
- *
- * @param {any} value - The value to be verified for strict equality.
- * @return {Object} assertions - A result of verifying strict equality.
- */
-const verifyIf = (value) => {
-  const assertions = {
-    isStrictEqual(expected) {
-      assert.strictEqual(value, expected);
-    },
-  };
-  return assertions;
-};
-
-/**
- * Compares the expected and returned values and logs whether the test passed or not.
- *
- * @param {any} title - The title of the test
- * @param {any} testFunction - The testFunction
- * @return {void}
- */
-const test = (title, testFunction) => {
-  try {
-    testFunction();
-    console.log(`✅ ${title} passed!`);
-  } catch {
-    console.log(`❌ ${title} failed!`);
-  }
-};
-
-test('addOvertime', () => {
-  const wanted = 2500;
-  const actual = addOvertime(2000, 500);
-
-  verifyIf(wanted).isStrictEqual(actual);
-});
-
-test('calculateDeductions', () => {
-  const wanted = 2000;
-  const actual = calculateDeductions(2500, 500);
-
-  verifyIf(wanted).isStrictEqual(actual);
-});
+export { somaHorasExtras, calculaDescontos, multiplicaDepoisDobra };
